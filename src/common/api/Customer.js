5f36859ff0f8cbcb771532b88c9d58eb8cefd537
Vicky Gupta <vicky.gupta@in.imshealth.com>
2021-05-16 21:05:40 +0530

    HomePage: Signup functionality with validations is added

--- src/common/api/Customer.js
+++ src/common/api/Customer.js
@@ -1,4 +1,4 @@
-import { LOGIN_CUSTOMER_URL } from "../common";
+import { LOGIN_CUSTOMER_URL, SIGNUP_CUSTOMER_URL } from "../common";
 
 /** Functional component to login the user */
 export function loginCustomer(encodedCredential) {
@@ -13,6 +13,28 @@ export function loginCustomer(encodedCredential) {
         let accessToken = resp.headers.get("access-token");
         localStorage.setItem("access-token", accessToken);
         // console.log(resp.headers.get('access-token'));
+        resp.json().then(res => {
+          return resolve(res);
+        });
+      })
+      .catch(error => {
+        reject(error);
+      });
+  });
+}
+
+/** Functional component to sign up the user */
+export function signUpCustomer(reqBody) {
+  return new Promise(function(resolve, reject) {
+    fetch(SIGNUP_CUSTOMER_URL, {
+      method: "POST",
+      headers:{
+        "Content-Type":"application/json"
+      },
+      body: JSON.stringify(reqBody)
+      
+    })
+      .then(resp => {
         resp.json().then(res => {
           return resolve(res);
         });
