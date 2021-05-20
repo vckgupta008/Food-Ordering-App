170ce3866b415b682221e70742896d1d4e07ecdc
Vicky Gupta <vicky.gupta@in.imshealth.com>
2021-05-20 21:27:32 +0530

    CheckoutPage: Adding Address form with validation and refreshing address list after adding

--- src/common/api/Address.js
+++ src/common/api/Address.js
@@ -1,4 +1,4 @@
-import { GET_ADDRESS_CUSTOMER_URL, GET_STATES_URL } from "../common";
+import { GET_ADDRESS_CUSTOMER_URL, GET_STATES_URL,ADD_ADDRESS_URL } from "../common";
 
 /** Functional component to fetch address of the customer */
 export function getAddressCustomer(accessToken) {
@@ -25,6 +25,28 @@ export function getStates() {
   return new Promise(function(resolve, reject) {
     fetch(GET_STATES_URL, {
       method: "GET"
+    })
+      .then(resp => {
+        resp.json().then(res => {
+          return resolve(res);
+        });
+      })
+      .catch(error => {
+        reject(error);
+      });
+  });
+}
+
+/** Functional component to add addresss */
+export function addAddress(reqBody, accessToken) {
+  return new Promise(function(resolve, reject) {
+    fetch(ADD_ADDRESS_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`
+      },
+      body: JSON.stringify(reqBody)
     })
       .then(resp => {
         resp.json().then(res => {
